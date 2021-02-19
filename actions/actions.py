426f0627fc6689d1e428644ae4f9f155c59bf5e7
prernaD <prernadave97@gmail.com>
2021-02-19 17:59:28 +0530

    added two actions

--- actions/actions.py
+++ actions/actions.py
@@ -46,3 +46,102 @@ class ActionLanguageSearch(Action):
             dispatcher.utter_message(text="lo siento, no pude entender")
 
         return []
+
+class ActionCountriesLanguageSearch(Action):
+
+    def name(self) -> Text:
+        return "action_countries_lang_search"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        data_path = os.path.join("data", "language_country", "country_lang.csv")
+        wals_data = pd.read_csv(data_path)
+        entities = list(tracker.get_latest_entity_values("language"))
+        wals_data['country'] = wals_data['country'].str.lower()
+        if len(entities) > 0:
+            print("I have an country entity")
+            query_lang = entities.pop()
+            query_lang = query_lang.lower()
+            print(query_lang)
+            
+            out_row = wals_data[wals_data["country"] == query_lang].to_dict("records")
+
+            if len(out_row) > 0:
+                out_row = out_row[0]
+                out_text = "Country %s have these languages %s\n" % (out_row["country"], out_row["language"])
+                dispatcher.utter_message(text = out_text)
+            else:
+                dispatcher.utter_message(text = "Lo sentimos, no tenemos registros para ese idioma. %s" % query_lang)
+        else:
+            dispatcher.utter_message(text="lo siento, no pude entender")
+
+        return []
+
+
+class ActionTenseSearch(Action):
+
+    def name(self) -> Text:
+        return "action_tense_search"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        data_path = os.path.join("data", "tense_csv", "tense.csv")
+        wals_data = pd.read_csv(data_path)
+        entities = list(tracker.get_latest_entity_values("language"))
+        wals_data['infinitive'] = wals_data['infinitive'].str.lower() 
+        if len(entities) > 0:
+            print("I have an tense entity")
+            query_lang = entities.pop()
+            query_lang = query_lang.lower()
+            print(query_lang)
+            
+            out_row = wals_data[wals_data["infinitive"] == query_lang].to_dict("records")
+
+            if len(out_row) > 0:
+                out_row = out_row[0]
+                out_text = "Verb %s is of type %s\n" % (out_row["infinitive"], out_row["tense"])
+                dispatcher.utter_message(text = out_text)
+            else:
+                dispatcher.utter_message(text = "Lo sentimos, no tenemos registros para ese idioma. %s" % query_lang)
+        else:
+            dispatcher.utter_message(text="lo siento, no pude entender")
+
+        return []
+
+
+class ActionGetPoliteness(Action):
+
+    def name(self) -> Text:
+        return "action_polite_search"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        data_path = os.path.join("data", "polite", "pronouns_spanish.xlsm")
+        wals_data = pd.read_excel(data_path)
+        entities = list(tracker.get_latest_entity_values("language"))
+        wals_data['English'] = wals_data['English'].str.lower()
+        print(entities)
+        if len(entities) > 0:
+            print("I have an politeness entity")
+            query_lang = entities.pop()
+            query_lang = query_lang.lower()
+            print(query_lang)
+            
+            out_row = wals_data[wals_data["English"] == query_lang].to_dict("records")
+
+            if len(out_row) > 0:
+                out_row = out_row[0]
+                out_text = "Politeness of english %s in spanish is %s\n" % (out_row["English"], out_row["Spanish"])
+                dispatcher.utter_message(text = out_text)
+            else:
+                dispatcher.utter_message(text = "Lo sentimos, no tenemos registros para ese idioma. %s" % query_lang)
+        else:
+            dispatcher.utter_message(text="lo siento, no pude entender")
+
+        return []
